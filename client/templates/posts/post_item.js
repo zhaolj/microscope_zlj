015820a8f8293550c030b6631ebbb14cc03df093
赵良晶 <zhaoliangjing@mocar.cn>
2016-04-30 21:41:56 +0800

    init

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,15 @@
+Template.postItem.helpers({
+	domain: function(){
+		var a = document.createElement('a');
+		a.href = this.url;
+		return a.hostname;
+	}
+});
+
+// Template.postItem.helpers({
+//   domain: function() {
+//     var a = document.createElement('a');
+//     a.href = this.url;
+//     return a.hostname;
+//   }
+// });
---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,40 @@
+var postsData = [
+	{
+		title: '摩卡新闻1号',
+		url: 'http://www.mocar.cn/'
+	},
+	{
+		title: '摩卡新闻2号',
+		url: 'http://www.mocar.cn/'
+	},
+		{
+		title: '摩卡新闻3号',
+		url: 'http://www.mocar.cn/'
+	},
+	{
+		title: '摩卡新闻4号',
+		url: 'http://www.mocar.cn/'
+	}
+];
+
+// Template.postsList.helpers({
+// 	post:postDate
+// });
+
+// var postsData = [
+//   {
+//     title: 'Introducing Telescope',
+//     url: 'http://sachagreif.com/introducing-telescope/'
+//   }, 
+//   {
+//     title: 'Meteor',
+//     url: 'http://meteor.com'
+//   }, 
+//   {
+//     title: 'The Meteor Book',
+//     url: 'http://themeteorbook.com'
+//   }
+// ];
+Template.postsList.helpers({
+  posts: postsData
+});
